5081ed2c3d27742716b9f74c3b91c2e6b8f57e68
shri <battulshrikantanand2329@gmail.com>
2026-04-22 12:06:35 +0530

    added generate table

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#write a python function which will take user input as a number and will print table of that number
+#git add
+#git commit -m 
+#git push 
 def table(number):
     for i in range(0,11):
         print(f"{number}* {i} = {number*i}")
